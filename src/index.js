1470db6c6f958be1878115729bf5c24d617866a7
Ali <ali.khoshtinattt@gmail.com>
2019-05-09 17:10:57 +0430

    handling both X and O alternatively

--- src/index.js
+++ src/index.js
@@ -2,20 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+
+function Square(props){
+  return (
+    <button 
+    className="square" 
+    onClick={props.onClick}>
+      {props.value }
+    </button>
+  );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor(props){
+    super(props)
+    this.state = {
+      square: Array(9).fill(null),
+      xIsNext: true,
+    };
   }
+
+  handleClick(i){
+    const squares = this.state.square.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      square:squares,
+      xIsNext : !this.state.xIsNext,
+    })
+  }
+
+  renderSquare(i) {
+    return <Square 
+    value = {this.state.square[i]}
+    onClick={() => this.handleClick(i)}
+    />;
+  } 
 
   render() {
     const status = 'Next player: X';
@@ -65,4 +86,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
